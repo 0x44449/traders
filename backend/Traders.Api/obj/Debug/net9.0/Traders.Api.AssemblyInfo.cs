c77450849fb9b1d36c5224e482c91f09d534e622
0x44449 <0x44449@gmail.com>
2025-08-04 10:14:20 +0900

    필요없는 파일 제거

--- backend/Traders.Api/obj/Debug/net9.0/Traders.Api.AssemblyInfo.cs
+++ backend/Traders.Api/obj/Debug/net9.0/Traders.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Traders.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce7af0215ba1b3d8430ddd15be6fb7b3027a1b7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Traders.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Traders.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
